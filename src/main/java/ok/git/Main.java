1bd78a5019d731211750252e4c1b89731e9b9db3
pathiknandi <pathik.nandi@tarento.com>
2022-10-17 18:25:49 +0530

    commit

--- src/main/java/ok/git/Main.java
+++
@@ -1,9 +0,0 @@
-package com.tarento.ok.git;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("hello");
-        System.out.println("Hii");
-    }
-}
