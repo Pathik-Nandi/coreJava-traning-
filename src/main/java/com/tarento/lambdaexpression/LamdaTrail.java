10329d4428860683b4019fce37500c2715e2ac11
pathiknandi <pathik.nandi@tarento.com>
2022-10-27 14:18:32 +0530

    Lambda Expression new changes

--- src/main/java/com/tarento/lambdaexpression/LamdaTrail.java
+++ src/main/java/com/tarento/lambdaexpression/LamdaTrail.java
@@ -2,12 +2,18 @@ package com.tarento.lambdaexpression;
 
 public abstract class LamdaTrail implements LambdaInterface {
     public static void main(String[] args) {
-       //Using Anonymous class approach
+
+        //--------Using C class Concrete approach----------
+        NewClass l1= new NewClass();
+        l1.emp(102,"nandi");
+
+       //---------Using Anonymous class approach------------
         LambdaInterface obj= new LambdaInterface() {
             @Override
             public void emp(int id, String companyname) {
-                System.out.println("number:"+id+"\n");
-                System.out.println("String is:"+companyname);
+                System.out.println("This is anonymous approach");
+                System.out.println("Register No: "+id);
+                System.out.println("Company Name:"+companyname+"\n");
 
             }
         };
@@ -15,7 +21,17 @@ public abstract class LamdaTrail implements LambdaInterface {
 
 //---------Using Lambda expression approach----
         LambdaInterface obj2 ;
-        obj2=(i,a)-> System.out.println("number"+i+ "\n String"+a);
+        System.out.println("This is lambda approach");
+        obj2=(i,a)-> System.out.println("Register No: "+i+ "\n Company name: "+a);
         obj2.emp(1089,"Tarento");
+    }
+    public static class NewClass implements LambdaInterface{
+        public void emp(int id, String companyname)
+        {
+            System.out.println("This is concrete approach");
+            System.out.println("Register:"+id);
+            System.out.println("Company:"+companyname+"\n");
+
+        }
     }
 }
