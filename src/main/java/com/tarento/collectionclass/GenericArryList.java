4c35d5ab4effa84168deb7f0876008925b0c4b89
pathiknandi <pathik.nandi@tarento.com>
2022-10-28 14:20:30 +0530

    Collection Class

--- src/main/java/com/tarento/collectionclass/GenericArryList.java
+++ src/main/java/com/tarento/collectionclass/GenericArryList.java
@@ -10,7 +10,7 @@ public class GenericArryList {
         laptoplist.add("Acer");
         laptoplist.add("Apple MackBook");
         laptoplist.add("Lenovo");
-        System.out.println("\n Original List of laptop: \n "+laptoplist);
+        System.out.println("\n Original List of laptop: \n " + laptoplist);
 
 
         Add add = new Add(laptoplist);
@@ -22,72 +22,62 @@ public class GenericArryList {
         Disjoint disjoint = new Disjoint(laptoplist);
         Num num = new Num();
         Remove remove = new Remove(laptoplist);
-
-
-
     }
 }
 
-class Add
-{
-  Add(List laptoplist)
-  {
-      Collections.addAll(laptoplist,"Asus","Xiaomi Laptops","HCL","Sony");
-      System.out.println("\n-------------Add Elements----------");
-      System.out.println("Laptop List after adding elements collection value:\n"+laptoplist);
-      System.out.println("Total No of elements: "+laptoplist.size()+"\n");
-
-  }
+class Add {
+    Add(List laptoplist) {
+        Collections.addAll(laptoplist, "Asus", "Xiaomi Laptops", "HCL", "Sony");
+        System.out.println("\n-------------Add Elements----------");
+        System.out.println("Laptop List after adding elements collection value:\n" + laptoplist);
+        System.out.println("Total No of elements: " + laptoplist.size() + "\n");
+    }
 }
-class Update
-{
-    Update(List laptoplist)
-    {
+
+class Update {
+    Update(List laptoplist) {
         try {
             System.out.println("\n-------------Replace Elements----------");
-            System.out.println("Before Replace elements \n"+laptoplist);
-            laptoplist.set(6,"Samsung Laptops");
-            System.out.println("After Update the list \n"+laptoplist);
-        }
-        catch (Exception e){
+            System.out.println("Before Replace elements \n" + laptoplist);
+            laptoplist.set(6, "Samsung Laptops");
+            System.out.println("After Update the list \n" + laptoplist);
+        } catch (Exception e) {
             System.out.println(e);
         }
-
     }
 }
-class Sort{
+
+class Sort {
     // Sorting according to default ordering using sort() method
-    Sort(List laptoplist){
+    Sort(List laptoplist) {
         Collections.sort(laptoplist);
         System.out.println("\n-------------Default sort ----------");
-        System.out.println("Sorting the Laptop List in ascending order:\n"+laptoplist);
+        System.out.println("Sorting the Laptop List in ascending order:\n" + laptoplist);
     }
 }
-class Binarysearch{
+
+class Binarysearch {
     // BinarySearch on the List
-    Binarysearch(List laptoplist){
+    Binarysearch(List laptoplist) {
         System.out.println("\n--------------Binary Search----------------");
         System.out.println("The index of HP is " + Collections.binarySearch(laptoplist, "HP"));
         System.out.println("The index of DELL  is " + Collections.binarySearch(laptoplist, "DELL"));
-
     }
 }
 
-class Reversesort{
+class Reversesort {
     // Sorting according to reverse ordering
-    Reversesort(List laptoplist){
+    Reversesort(List laptoplist) {
         Collections.sort(laptoplist, Collections.reverseOrder());
         System.out.println("\n--------------reverse order-------------");
-        System.out.println("Sorting the laptop List in reverse order:\n"+laptoplist);
-
+        System.out.println("Sorting the laptop List in reverse order:\n" + laptoplist);
     }
-
 }
 
-class Copylist{
-    Copylist(List laptoplist){
+class Copylist {
+    Copylist(List laptoplist) {
         System.out.println("\n-------------copying----------");
-        System.out.println("List before copying:\n"+laptoplist);
+        System.out.println("List before copying:\n" + laptoplist);
         List<Object> microprocessor = new ArrayList<>();
         microprocessor.add(" Altera");
         microprocessor.add("AMD");
@@ -96,34 +86,34 @@ class Copylist{
         microprocessor.add("Hitachi");
 
         // list of microprocessors company
-        System.out.println("\n list of microprocessors company: "+microprocessor);
+        System.out.println("\n list of microprocessors company: " + microprocessor);
 
         // Copy the elements from one list to another
         Collections.copy(laptoplist, microprocessor);
-        System.out.println("\n Copying microprocessors company to Laptoplist:\n"+laptoplist);
+        System.out.println("\n Copying microprocessors company to Laptoplist:\n" + laptoplist);
 
         // sorting the modified list
         Collections.sort(laptoplist);
-        System.out.println("\nSorting the Anime List ascending order:\n"+laptoplist);
-
+        System.out.println("\nSorting the Anime List ascending order:\n" + laptoplist);
     }
 }
-class Disjoint{
+
+class Disjoint {
     // Check if disjoint or not, if they have no elements in common
-    Disjoint(List laptoplist){
+    Disjoint(List laptoplist) {
         List<String> newlist = new ArrayList<>();
         newlist.add("Altera");
         newlist.add("AMD");
         newlist.add("Acer");
         System.out.println("\n-------------Disjoint----------");
-        System.out.println("laptop List: "+laptoplist);
-        System.out.println("New List: "+newlist);
-        System.out.println("Checking if the above 2 list are disjoint or not: "+Collections.disjoint(laptoplist,newlist));
+        System.out.println("laptop List: " + laptoplist);
+        System.out.println("New List: " + newlist);
+        System.out.println("Checking if the above 2 list are disjoint or not: " + Collections.disjoint(laptoplist, newlist));
     }
-
 }
-class Num{
-    Num(){
+
+class Num {
+    Num() {
         List<Integer> list = new ArrayList<Integer>();
         list.add(46);
         list.add(38);
@@ -133,13 +123,13 @@ class Num{
         list.add(12);
         System.out.println("\n-------------Sort Max and Min value----------");
         System.out.println(list);
-        System.out.println("Value of maximum element from the collection: "+Collections.max(list));
-        System.out.println("Value of minimum element from the collection: "+Collections.min(list));
+        System.out.println("Value of maximum element from the collection: " + Collections.max(list));
+        System.out.println("Value of minimum element from the collection: " + Collections.min(list));
     }
 }
-class  Remove{
-    Remove(List laptoplist)
-    {
+
+class Remove {
+    Remove(List laptoplist) {
         System.out.println("\n-------------Delete----------");
         System.out.println(laptoplist);
         System.out.println("\n Delete Particular elements by name/object(DELL)");
@@ -147,9 +137,9 @@ class  Remove{
         System.out.println(laptoplist);
         System.out.println("\n Delete Particular elements by Index number:3");
         laptoplist.remove(3);
-        System.out.println(laptoplist+"\n");
+        System.out.println(laptoplist + "\n");
         System.out.println("clears everything from index 3 ");
         laptoplist.subList(3, laptoplist.size()).clear();
-            System.out.println(laptoplist);
+        System.out.println(laptoplist);
     }
 }
